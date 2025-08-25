dabac48670578cdf4c3405d30d0acc15a62e4253
Cestamir <cestmirpavlasek@gmail.com>
2025-08-25 21:18:02 +0200

    Added notes inside the app.tsx with basic structure of the upcoming portofolio website, which will be created in this repository.

--- my-portofolio/src/App.tsx
+++ my-portofolio/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Expand your team</h1>
+        {/* NAVBAR, (work, contact, vision, experience), focus, hide when scrolling down appears when scrolling up, oval rectangle shape with glass border
+        
+
+        HOMEPAGE, eye catching visual or background with my name and titul, when scrolling down or hitting the arrow down, immediatly switches to the first item from navbar section
+
+        WORK
+
+        CONTACT github,linkedn,email,X + About yourself ? maybe diffrent section.
+
+        VISION, i will become a software developer possibly software enginer, interests - physics,astronomy,sound,lifestyle,biohacking,hacking etc.
+
+        EXPERIENCE, technologies used, technologies working with, technologies mastered or really prollific, mention courses and books !!
+
+        BONUS, make clickable elements interactive and add sounds and visual effects like blur or glitter, edit basic cursor, add buttons for sound on/off and performance 
+
+
+        */}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
