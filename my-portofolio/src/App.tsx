7aefb337d5b7e9e99c78b0ad9f8d77ccfd10d966
Cestamir <cestmirpavlasek@gmail.com>
2025-08-29 18:21:31 +0200

    add noted in to the code for future edits, need to work on overall vision of the website itself

--- my-portofolio/src/App.tsx
+++ my-portofolio/src/App.tsx
@@ -16,10 +16,15 @@ function App() {
       <main>
         <Navbar/>
         <HomePage/>
+        <div className='stripes'></div>
         <WorkPage/>
+        <div className='stripes'></div>
         <ContactPage/>
+        <div className='stripes'></div>
         <VisionPage/>
+        <div className='stripes'></div>
         <ExperincePage/>
+        <div className='stripes'></div>
         <Bonus/>
         {/* NAVBAR, (work, contact, vision, experience), focus, hide when scrolling down appears when scrolling up, oval rectangle shape with glass border
         
---
+++ my-portofolio/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = ({}) => {
+  return (
+    <div id='card'>
+        {/* simple card component need to change overall layout for better composition */}
+    </div>
+  )
+}
+
+export default Card
