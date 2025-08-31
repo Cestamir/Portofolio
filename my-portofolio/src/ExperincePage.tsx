e933b51328631b584693d8dcdc9f28c9ffba5394
Cestamir <cestmirpavlasek@gmail.com>
2025-08-31 19:00:33 +0200

    added display option when element in the experience page is clicked

--- my-portofolio/src/ExperincePage.tsx
+++ my-portofolio/src/ExperincePage.tsx
@@ -1,20 +1,53 @@
 
+import { useState } from 'react'
 import Headline from './components/Headline'
 import { expItemsList } from './index'
 
 
+interface GridItem {
+  id: number,
+  name: string,
+  info: string,
+  skills: string,
+  status: string
+}
+
 const ExperincePage = () => {
+
+  const [selectedItem,setSelectedItem] = useState<GridItem | null>(null);
+
+  const handleItemClick = (item : GridItem) => {
+    setSelectedItem(item);
+  }
+
+  const closeItemDisplay = () => {
+    console.log('Closing, selectedItem was:', selectedItem);
+    setSelectedItem(null);
+    console.log('selectedItem set to null');
+  }
+
   return (
     <section>
         <Headline text='My experience' size={64} />
-        <div className='exp-grid'>
+        <div style={{display: selectedItem ? "none" : "grid"}} className='exp-grid'>
           {expItemsList.map((item) => (
-            <div key={item.name} className='grid-exp-item'>
+            <div onClick={() => handleItemClick(item)} key={item.id} className='grid-exp-item'>
               {item.name}
               {/* need to add simple logo pictures to the frame, when it will be hovered or clicked show more info (skills,info,status) */}
             </div>
           ))}
         </div>
+        {selectedItem && (
+          <div className='exp-item-display'>
+            <button onClick={closeItemDisplay} className='close-btn'>
+              x
+            </button>
+            <h1>{selectedItem.name}</h1>
+            <p>{selectedItem.info}</p>
+            <h3>{selectedItem.skills}</h3>
+            <h4>{selectedItem.status}</h4>
+          </div>
+        )}
     </section>
   )
 }
--- my-portofolio/src/index.ts
+++ my-portofolio/src/index.ts
@@ -2,16 +2,16 @@
 export const navItemsList = [{name: "Contact",color: "blue"},{name: "Work",color: "red"},{name: "Vision",color: "grey"},{name:"Experience",color: "purple"}]
 
 export const expItemsList = [
-    {name: "ItNetwork.cz",info: "1st official course i have completed in Czech Republic focused mainly on basic web development and Javascript itslef",skills: "HTML,CSS,Javasript,CSS frameworks",status: "Completed"},
-    {name: "freeCodeCamp",info: "The best free course on frontend development in the world, my 2nd course where i came across diffrent style of learning new skills in tech field",skills: "Docker,SQL,Javascript frameworks",status: "In progress"},
-    {name: "Audio",info: "I my current job i am able to listen to various podcast as JS syntax,JS jabber, Lex Friedman, The programming podcast, freeCodeCamp podcast, Frontend fire, The standup from Primeagen and many more..",skills: "Most useful resources in tech world and critical news about what is happening",status: "Still listening :D"},
-    {name: "Books",info: "Tech books and self-development books",skills: "philosophy",status: "Completed"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
-    {name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 1,name: "ItNetwork.cz",info: "1st official course i have completed in Czech Republic focused mainly on basic web development and Javascript itslef",skills: "HTML,CSS,Javasript,CSS frameworks",status: "Completed"},
+    {id: 2,name: "freeCodeCamp",info: "The best free course on frontend development in the world, my 2nd course where i came across diffrent style of learning new skills in tech field",skills: "Docker,SQL,Javascript frameworks",status: "In progress"},
+    {id: 3,name: "Audio",info: "I my current job i am able to listen to various podcast as JS syntax,JS jabber, Lex Friedman, The programming podcast, freeCodeCamp podcast, Frontend fire, The standup from Primeagen and many more..",skills: "Most useful resources in tech world and critical news about what is happening",status: "Still listening :D"},
+    {id: 4,name: "Books",info: "Tech books and self-development books",skills: "philosophy",status: "Completed"},
+    {id: 5,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 6,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 7,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 8,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 9,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
+    {id: 10,name: "English",info: "I've been learning since 2006",skills: "language",status: "lifetime work"},
 ]
 
 export const projects = [
